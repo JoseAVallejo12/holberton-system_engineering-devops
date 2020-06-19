165ea86426ce7aaf46b66eaec4f34c501ff7e02c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-19 10:30:05 -0500

    first commit and create files task and readme

---
+++ 0x04-loops_conditions_and_parsing/tes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" class test """
+class Hola(object):
+    """ class coment """
+    def __init__(self):
+       for n in range(10):
+           print("mi casa tiene {} cuertos".format(n)
---
+++ 0x04-loops_conditions_and_parsing/test.c
@@ -0,0 +1,12 @@
+#include <stdio.c>
+/**
+ *
+ *
+ */
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("las cosas de la vida");
+	}
+}
