66523d77ebe92195c2dd818fe4c36a2853384ffd
josevallejo1984 <@16Se121984$>
2020-08-03 22:48:34 +0000

    create task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,9 @@
 # create an file in /tmp with owner, permisions and content
 file { 'tmp/holberton'
-    ensure = 
+ensure  => present,
+path    => '/tmp/holberton',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
