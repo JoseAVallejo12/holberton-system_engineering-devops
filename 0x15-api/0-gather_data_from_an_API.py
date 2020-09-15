e3cdcb29ca34a6f74f3c407ceefe0e407fef73c2
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-14 19:55:35 -0500

    fix output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+returns information about his/her list progress."""
 
 if __name__ == "__main__":
     import csv
@@ -8,19 +8,20 @@ if __name__ == "__main__":
     import requests
 
     URI_base = 'https://jsonplaceholder.typicode.com'
-    tasks_query = {"userId": 1}
-    user = requests.get(str(URI_base + "/user/" + argv[1])).json()
-    tasks = requests.get(str(URI_base + "/todos"), params=tasks_query)
+    tasks_query = {"userId": argv[1]}
+    user_query = {"id": argv[1]}
+    user = requests.get(str(URI_base + "/users/"), params=user_query).json()
+    tasks = requests.get(str(URI_base + "/todos"), params=tasks_query).json()
     user_task_done = []
 
-    for task in tasks.json():
+    for task in tasks:
         if (task.get("completed")):
             user_task_done.append(task.get("title"))
 
     print('Employee {} is done with tasks({}/{}):'.format(
-        user.get("name"),
+        user[0].get("name"),
         len(user_task_done),
-        (len(tasks.json()) - len(user_task_done))
+        len(tasks)
     ))
     for val in user_task_done:
         print("\t ", end="")
