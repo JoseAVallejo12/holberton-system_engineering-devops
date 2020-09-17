0ef5e8fa40ffa751e4132dbece9baeb72d0e4973
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-17 18:30:57 -0500

    change to recursion method

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,21 +5,16 @@ all hot articles for a given subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], params={}):
     """recurse functions"""
     uri = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "holberton"}
-    data = requests.get(uri, headers=headers).json().get("data")
-    count = data.get("dist")
-
-
-    while data.get("after") is not None:
-        params = {"after": data.get("after")}
-        data = requests.get(
-            uri,
-            headers=headers,
-            params=params).json().get("data")
-        count += data.get("dist")
-    if count is 0:
+    data = requests.get(uri, headers=headers, params=params).json().get("data")
+    params = {"after": data.get("after")}
+    hot_list.append(data.get("dist"))
+    if data.get("after") is not None:
+        recurse(subreddit, hot_list, params)
+    if sum(hot_list) is not 0:
+        return range(0, sum(hot_list))
+    else:
         return None
-    return range(0, count)
