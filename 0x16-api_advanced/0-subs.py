d506c479c1d79a9425787c6f12784983c3b50c99
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-17 12:20:55 -0500

    create tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a function that queries the Reddit API and returns the number of subscribers."""
+import requests
+def number_of_subscribers(subreddit):
+    """Get number of suscriptor in redit
+    Args:
+        subreddit (str): subreddit or topic to search subs
+    Returns:
+        [int]: Number subscriber or 0 else not exist
+    """
+    headers = {"User-Agent": "holberton"}
+    res = requests.get('https://api.reddit.com/r/{}/about'.format(subreddit), headers=headers)
+    return res.json().get('data', {}).get("subscribers", 0)
